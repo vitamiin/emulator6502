e5f3e979104bf410facdfa6df00ccb9a1e28519f
vitamiin <mailthis2tom@gmail.com>
2021-11-29 21:26:11 +0200

    declared registers, flags and basic types

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,27 @@
 #include <iostream>
 
+
+struct CPU 
+{
+  using Byte = unsigned char;
+  using Word = unsigned short;
+
+  Word PC;        // program counter / instruction pointer
+  Word SP;        // stack pointer
+  
+  Byte A, X, Y;   // registers
+  
+  // status flags
+  Byte C : 1;   // carry flag
+  Byte Z : 1;   // zero flag
+  Byte I : 1;   // interrupt disable
+  Byte D : 1;   // decimal mode
+  Byte B : 1;   // break command
+  Byte V : 1;   // overflow flag
+  Byte N : 1;   // negative flag
+};
+
+
 int main(){
-  std::cout << "Hello World!" << std::endl;
+  return 0;
 }
